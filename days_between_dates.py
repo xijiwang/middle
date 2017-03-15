3db75be0ef3447b02a9730d62425485695fe4364
xijiwang <xjwang.coder@gmail.com>
2017-03-15 23:30:26 +0800

    finding the days between two dates

--- days_between_dates.py
+++ days_between_dates.py
@@ -13,65 +13,67 @@ def days_between_dates(y1,m1,d1,y2,m2,d2):
 	temp = 0
 	yrs = 0
 	temp_year = 0
-	if is_leap_year(y1) == True:
-		temp = days_in_month_of_leap_year[m1] - d1 + 1
-		i = m1 + 1
-		while i < 13:
-			temp += days_in_month_of_leap_year[i]
+	if y1 != y2:
+		if is_leap_year(y1) == True:
+			temp = days_in_month_of_leap_year[m1] - d1 + 1
+			i = m1 + 1
+			while i < 13:
+				temp += days_in_month_of_leap_year[i]
+				i += 1
+		else:
+			temp = days_in_month[m1] - d1 + 1
+			i = m1 + 1
+			while i < 13:
+				temp += days_in_month[i]
+				i += 1
+		yrs = y2 - y1 - 1
+		temp_year = y1 + 1
+		while temp_year < y2:
+			if is_leap_year(temp_year) == True:
+				temp += 366
+			else:
+				temp += 365
+			temp_year += 1
+		i = 1
+		while i < m2:
+			if is_leap_year(y2) == True:
+				temp += days_in_month_of_leap_year[i]
+			else:
+				temp += days_in_month[i]
 			i += 1
+		temp += d2
 	else:
-		temp = days_in_month[m1] - d1 + 1
-		i = m1 + 1
-		while i < 13:
-			temp += days_in_month[i]
-			i += 1
-	yrs = y2 - y1 - 1
-	temp_year = y1 + 1
-	while temp_year < y2:
-		if is_leap_year(temp_year) == True:
-			temp += 366
+		if m1 == m2:
+			temp += d2 - d1
 		else:
-			temp += 365
-		temp_year += 1
-	i = 1
-	while i < m2:
-		if is_leap_year(y2) == True:
-			temp += days_in_month_of_leap_year[i]
-		else:
-			temp += days_in_month[i]
-		i += 1
-	temp += d2
+			if is_leap_year(y1) == True:
+				temp += days_in_month_of_leap_year[m1] - d1 + 1
+			else:
+				temp += days_in_month[m1] - d1 + 1
+		j = m1 + 1
+		while j < m2:
+			if is_leap_year(y2) == True:
+				temp += days_in_month_of_leap_year[j]
+			else:
+				temp += days_in_month[j]
+			j += 1
+		
+
+
+
+
+
 	return (temp,yrs)
 
+# print days_between_dates(2006,5,19,2009,3,30)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-print days_between_dates(2006,5,19,2009,3,30)
-
-print days_between_dates(2010,6,11,2012,4,18)
+# print days_between_dates(2010,6,11,2012,4,18)
 #>> 677
 print days_between_dates(1999,3,9,1999,12,5)
 #>> 271
-print days_between_dates(2010,7,9,2033,5,4)
+# print days_between_dates(2010,7,9,2033,5,4)
 #>> 8335
-print days_between_dates(2004,5,19,2005,4,17)
+# print days_between_dates(2004,5,19,2005,4,17)
 #>> 333
-print days_between_dates(2012,6,2,2012,6,25)
+# print days_between_dates(2012,6,2,2012,6,25)
 #>> 23
---
+++ test.py
@@ -0,0 +1,36 @@
+import days_between_dates
+# def is_leap_year(y):
+# 	if y % 4 == 0:
+# 		return True
+# 	else:
+# 		return False
+# days_in_months = (0,31,28,31,30,31,30,31,31,30,31,30,31)
+# days_in_months_of_leap_year = (0,31,29,31,30,31,30,31,31,30,31,30,31)
+
+# def days_in_dates(y1,m1,d1,y2,m2,d2):
+# 	temp = 0
+# 	if m1 != m2:
+# 		if is_leap_year(y1) == True:
+			
+# 		else:
+
+
+
+
+# 	else:
+
+
+# 	return temp
+
+print days_between_dates(2006,5,19,2009,3,30)
+
+print days_between_dates(2010,6,11,2012,4,18)
+#>> 677
+print days_between_dates(1999,3,9,1999,12,5)
+#>> 271
+print days_between_dates(2010,7,9,2033,5,4)
+#>> 8335
+print days_between_dates(2004,5,19,2005,4,17)
+#>> 333
+print days_between_dates(2012,6,2,2012,6,25)
+#>> 23
